8d6ac735c9609d74e8c096469f1ffec53bb2475d
Bjorn Carlsson <bjorn.r.carlsson@schneider-electric.com>
2018-03-02 15:14:55 +0100

    loading ajax...

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockAuthorApi';
+import {beginAjaxCall} from './ajaxStatusActions';
 
 export function loadAuthorsSuccess(authors) {
   return {type: types.LOAD_AUTHORS_SUCCESS, authors};
@@ -8,6 +9,7 @@ export function loadAuthorsSuccess(authors) {
 export function loadAuthors() {
   return function(dispatch) {
     return courseApi.getAllAuthors().then(authors => {
+      dispatch(beginAjaxCall());
       dispatch(loadAuthorsSuccess(authors));
     }).catch(error => {
       throw(error);
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -17,6 +17,11 @@ class ManageCoursePage extends React.Component {
     this.saveCourse = this.saveCourse.bind(this);
   }
 
+  componentWillReceiveProps(nextProps){
+    if (this.props.course.id == nextProps.course.id) return;
+    this.setState({course: Object.assign({}, nextProps.course)});
+  }
+
   updateCourseState(event) {
     const field = event.target.name;
     let course = Object.assign({}, this.state.course);
@@ -51,9 +56,20 @@ ManageCoursePage.propTypes = {
   authors: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
 };
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id == id);
+  if (course.length) return course[0];
+  return null;
+}
 
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id; //from the path '/course/:id'
+
   let course = { id: '', watchHref: '', title: '', authorId: '', length: '', category: '' };
+  if (courseId && state.courses.length){
+    course = getCourseById(state.courses, courseId);
+  }
+
   const mappedAuthors = state.authors.map(author => {
     return {
       value: author.id,
@@ -62,7 +78,7 @@ function mapStateToProps(state, ownProps) {
   });
 
   return {
-    courses: course,
+    course: course,
     authors: mappedAuthors
   };
 }
